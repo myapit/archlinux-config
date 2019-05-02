86bcddffd3e6fedf13ac703688957e71af530a7e
myapit <hafeez.cs@gmail.com>
2019-05-02 10:19:53 +0800

    Fully working i3 status bar in VM arch

---
+++ config-vm-arch/.config/cmus/lib.pl
@@ -0,0 +1,7 @@
+/home/apit/Music/Alan Walker - Faded-60ItHLz5WEA.mp3
+/home/apit/Music/Amira Othman  - Hati-Hati (OST SHA DAN SHAH)-2gsuHMkFnyQ.mp3
+/home/apit/Music/Fynn Jamal - Terbang Tunduk Official Music Video-yrki3YjIrdQ.mp3
+/home/apit/Music/Tasha Manshahar Feat. Viral - Oh No (Official Music Video)-3ViVxFu98wE.mp3
+/home/apit/Music/Wani Feat. Juzzthin - Alhamdulillah (Official Music Video)-T0saZo4OcQ8.mp3
+/home/apit/Music/Wani Feat. Juzzthin - Lepaskan (Official Music Video)-63lFfy5Cmv4.mp3
+/home/apit/Music/Wany Hasrita - Menahan Rindu (OST Lelaki itu pemilik hatiku - Official Music Video)-89MEnvcS2Qg.mp3
